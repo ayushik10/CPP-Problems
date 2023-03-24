0b04790b861c06807e8f8678f17c901e9cd3f69e
Ayushi Kumari <115365818+ayushik10@users.noreply.github.com>
2023-03-24 22:24:08 +0530

    Add files via upload

---
+++ CodeChef_Problems/CANDIVIDE.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	int N;
+	cin >> N;
+	
+	if(N%3 == 0){
+	    cout << "YES" << endl;
+	}
+	else
+	cout << "NO" << endl;
+	}
+	return 0;
+}
