3ba681174de05d8d0635f957cb7a30544483f189
Ayushi Kumari <115365818+ayushik10@users.noreply.github.com>
2023-10-12 21:14:49 +0530

    Create Solution.java

---
+++ LeetCode Problems/2873.-Maximum Value of an Ordered Triplet I/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        long long ans = 0;
+
+        for(int i=0; i<nums.size()-2; i++) {
+          for(int j=i+1; j<nums.size()-1; j++) {
+            for(int k=j+1; k<nums.size(); k++) {
+               ans = max(ans, ((long long) (nums[i]-nums[j])*(nums[k])));
+            }
+          }
+        }
+        return ans;
+    }
+};
